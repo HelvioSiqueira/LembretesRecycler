296ba6950267417efe094cbdb94df4cb1fe8cb0a
HelvioSiqueira <helviosiqueira@outlook.com>
2022-06-01 16:16:11 -0300

    commit comum(Colocando o sppiner)

--- app/src/main/java/com/example/lembretesrecycler/MainActivity.kt
+++ app/src/main/java/com/example/lembretesrecycler/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.lembretesrecycler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.lembretesrecycler.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +15,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initSpinner()
+        spnPrioridades.setSelection(3)
+    }
+
+    private fun initSpinner(){
+        val prioridades = arrayOf("Urgente", "Importante", "Irrelevante","Fixo")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, prioridades)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spnPrioridades.adapter = adapter
     }
 }
